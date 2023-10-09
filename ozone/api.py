ba17a0fc510f75ba7eb31ce26d7b5d9a9036c1ae
msperryucsd <msperry@ucsd.edu>
2023-10-09 13:48:57 -0700

    remove nativesystem support

--- ozone/api.py
+++ ozone/api.py
@@ -1,5 +1,5 @@
 from ozone.classes.ODEProblem import ODEProblem
 
-from ozone.classes.NativeSystem import NativeSystem
+# from ozone.classes.NativeSystem import NativeSystem
 
 from ozone.classes.Wrap import Wrap
